36020b5a34314f54378ecad8bf9635bd7ea9e2f2
Chakshu Gupta <chakshugupta2000@gmail.com>
2020-02-09 12:39:49 +0530

    Add BIRA and RSI

---
+++ BIRA.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+import backtrader as bt
+
+ADX = 25
+QTY = 5
+
+
+class BiraStrategy(bt.Strategy):
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
+    def _init_(self):
+        self.adx = bt.ind.ADX()
+        self.dmiplus, self.dmimin = bt.ind.PlusDI(), bt.ind.MinusDI()
+        self.dmi = bt.ind.DMI()
+        self.crossoverdmi = bt.ind.CrossOver(self.dmimin, self.dmiplus)
+        self.ich = bt.ind.Ichimoku()
+        self.rsi = bt.ind.RelativeStrengthIndex()
+        self.crossover = bt.ind.CrossOver(self.data0, self.ich.l.tenkan_sen)
+
+
+    def next(self):
+        print(self.datas[0].datetime.date(0))
+
+        self.tenkan_sen = self.ich.l.tenkan_sen[0]
+        self.kijun_sen = self.ich.l.kijun_sen[0]
+        self.senkou_span_a = self.ich.l.senkou_span_a[0]
+        self.senkou_span_b = self.ich.l.senkou_span_b[0]
+        self.ichi=self.ich.l.chikou_span[0]
+        print(self.tenkan_sen,self.kijun_sen,self.senkou_span_a,self.senkou_span_b)
+        print(self.senkou_span_a, self.datas[0].close[0])
+        print(self.getsizer())
+
+
+        if self.dmiplus > self.dmimin and self.adx[0]>25:
+            self.buy(size=QTY)
+        elif self.datas[0].open > self.ichi or self.rsi>70:
+            self.close(size=3*QTY)
+
+        if self.datas[0].datetime.date(0) == datetime(2019, 1, 1).date():
+            self.close()
+
+
+
+cerebro = bt.Cerebro(stdstats=False)
+cerebro.addobserver(bt.observers.BuySell)
+
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+print('Starting Portfolio Cash: %.2f' % cerebro.broker.get_cash())
+
+cerebro.addstrategy(BiraStrategy)
+cerebro.adddata(bt.feeds.YahooFinanceData(dataname='^NSEI', fromdate=datetime(2013, 1, 1), todate=datetime(2019, 1, 2))bt.feeds.YahooFinanceData(dataname='^NSEI', fromdate=datetime(2013, 1, 1), todate=datetime(2019, 1, 2)))
+cerebro.broker.set_cash(200000)
+cerebro.run()
+
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+print('Final Portfolio Cash: %.2f' % cerebro.broker.get_cash())
+
+cerebro.plot()
---
+++ RSI.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import backtrader as bt
+
+
+class RelativeStrengthIndexStrategy(bt.SignalStrategy):
+    def _init_(self):
+        self.index = bt.ind.RelativeStrengthIndex()
+
+    def next(self):
+        if self.index < 40 and self.datas[0].close[0] > self.datas[-1].close[-1]:
+            self.buy(size=0.5)
+
+        elif self.index > 70 and self.datas[0].close[0] < self.datas[-1].close[-1]:
+            self.close(size=0.5)
+
+        if self.datas[0].datetime.date(0) == datetime(2020, 1, 29).date():
+            self.close()
+
+
+cerebro = bt.Cerebro(stdstats=False)
+cerebro.addobserver(bt.observers.BuySell)
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+print('Starting Portfolio Cash: %.2f' % cerebro.broker.get_cash())
+cerebro.addstrategy(RelativeStrengthIndexStrategy)
+data0 = bt.feeds.YahooFinanceData(dataname='^NSEI', fromdate=datetime(2016, 1, 1),
+                                  todate=datetime(2020, 1, 30))
+
+cerebro.adddata(data0)
+cerebro.run()
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+print('Final Portfolio Cash: %.2f' % cerebro.broker.get_cash())
+cerebro.plot()
